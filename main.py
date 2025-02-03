78c931966b913e62fbc9b236f8fb9f62e4b2d901
kingmartinlutar <challengegaming98@gmail.com>
2025-02-03 14:29:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 from threading import Thread
 from pyrogram import Client, filters
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import API_ID, API_HASH, BOT_TOKEN
+from .env import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
 
